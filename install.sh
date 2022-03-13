359392c54df278aa513057bccaf393ba1c8d8795
Jeff Stout <stoutjd@nih.gov>
2022-03-13 17:51:02 -0400

    Added submodule download for mne_bids_pipeline

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
+# Download mne_bids_pipeline submodule
+echo Downloading mne_bids_pipeline
+git submodule init
+git submodule update
+
+#Install mamba to increase speed of conda creation
+echo Installing standard MNE graphical configuration
 conda install conda-forge::mamba -y 
 conda create -n enigma_meg -y
 conda activate enigma_meg
-mamba install conda-forge::mne -y
+mamba install pip conda-forge::mne -y
+
+#Install mne_bids_pipeline reqs
+echo Installing additional mne_bids_requirements
+pip install ./mne_bids_pipeline/requirements.txt
 

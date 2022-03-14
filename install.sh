ec857a33eefb6394a381e8751e98d89e326def31
Jeff Stout <stoutjd@nih.gov>
2022-03-14 16:04:36 -0400

    FIX - perform full install from conda yml file

--- install.sh
+++ install.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
-if [[ $- == *i* ]]
-then
-	echo Shell is properly in interactive mode
-else
-	echo Error: Shell needs to be invoked using interactive flag
-	echo Without the -i, bash cannot access conda activate in subshell
-	echo bash -i $0
-	exit 1
-fi
 
 # Download mne_bids_pipeline submodule
 echo Downloading mne_bids_pipeline
 git submodule init
 git submodule update
 
-#
+#Get full path for conda - so it doesn't have to be activated
 conda_call=$(which conda) 
-
 
 #Install mamba to increase speed of conda creation
 echo Installing standard MNE graphical configuration
 ${conda_call} install conda-forge::mamba -y 
-#conda create -p ./enigma_meg -y
-#conda activate $(pwd)/enigma_meg
 mamba_call=$(which mamba)
-${mamba_call} create -p $(pwd)/enigma_meg pip ipython conda-forge::mne -y
+${mamba_call} env create -p $(pwd)/enigma_meg -f ./environment.yml 
 
 #Install mne_bids_pipeline reqs
 echo Installing additional mne_bids_requirements
 pip_call=$(pwd)/enigma_meg/bin/pip
-${pip_call} install -r ./mne-bids-pipeline/requirements.txt
 
 #Copy additional scripts to mne_bids_pipeline
 cp $(pwd)/add_ons/_8*.py $(pwd)/mne-bids-pipeline/scripts/source/

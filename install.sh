0cbe3f069d38043d709a530a36cb2b47dc4e25ad
Jeff Stout <stoutjd@nih.gov>
2022-03-13 21:18:41 -0400

    Make conda install a local install

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ git submodule update
 #Install mamba to increase speed of conda creation
 echo Installing standard MNE graphical configuration
 conda install conda-forge::mamba -y 
-conda create -n enigma_meg -y
-conda activate enigma_meg
+conda create -p ./enigma_meg -y
+conda activate $(pwd)/enigma_meg
 mamba install pip conda-forge::mne -y
 
 #Install mne_bids_pipeline reqs
